852201ff09cebe8dd3843d7433d889a3653ee25a
kimdaye77 <kimdaye77@naver.com>
2021-10-29 14:51:05 +0900

    update skills

---
+++ static/js/common.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $('#python .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"81%"
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('#c .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"88%"
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('#cpp .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"92%"
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('#java .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"85%"
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    $('#html .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"82%"
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('#css .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"70%"
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('#js .guage').each(function () {
+        var $this = $(this);
+        $this.animate({
+            width:"60%"
+        });
+    });
+});
